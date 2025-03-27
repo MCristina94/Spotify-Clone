c4d491ae91309c2900da78cc5d1e653296632f99
Maureen Garcia <cmaureen1908@gmail.com>
2025-03-27 16:47:47 -0500

    finished footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,12 +7,17 @@ import {
   faForward,
   faBackward,
   faRepeat,
+  faMicrophone,
+  faList,
+  faDesktop,
+  faVolumeHigh,
+  faUpRightAndDownLeftFromCenter,
 } from "@fortawesome/free-solid-svg-icons";
 import { faCirclePlay as playIcon } from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
   return (
-    <div className="h-[100px] p-5 flex flex-row ">
+    <div className="h-[100px] p-5 flex flex-row justify-between mr-10">
       <div className="mx-5 flex flex-row">
         <img src={imageSabrina} className="w-[60px] h-[60px] rounded-md" />
         <div className="flex flex-col  mx-5 justify-center ">
@@ -26,30 +31,66 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="flex flex-row gap-2 content-center items-center">
-        <FontAwesomeIcon
-          icon={faShuffle}
-          className=" text-[#2b2b35]  text-[20px] mx-3"
-        />
-        <FontAwesomeIcon
-          icon={faBackward}
-          className=" text-[#7d7d7d] text-[20px] mx-3"
-        />
-        <FontAwesomeIcon
-          icon={playIcon}
-          className=" text-white text-[30px] mx-3"
-        />
-        <FontAwesomeIcon
-          icon={faForward}
-          className=" text-[#7d7d7d] text-[20px] mx-3"
-        />
-        <FontAwesomeIcon
-          icon={faRepeat}
-          className=" text-[#2b2b35] text-[20px] mx-3"
-        />
+      <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-row gap-4 items-center justify-center">
+          <FontAwesomeIcon
+            icon={faShuffle}
+            className="text-[#2b2b35] text-[20px] cursor-pointer"
+          />
+          <FontAwesomeIcon
+            icon={faBackward}
+            className="text-[#7d7d7d] text-[20px] cursor-pointer"
+          />
+          <FontAwesomeIcon
+            icon={playIcon}
+            className="text-white text-[30px] cursor-pointer"
+          />
+          <FontAwesomeIcon
+            icon={faForward}
+            className="text-[#7d7d7d] text-[20px] cursor-pointer"
+          />
+          <FontAwesomeIcon
+            icon={faRepeat}
+            className="text-[#2b2b35] text-[20px] cursor-pointer"
+          />
+        </div>
+
+        <div className="w-[500px] h-[5px] bg-[#ccc] relative mt-3 rounded-full overflow-hidden">
+          <div className="w-[50%] h-full bg-white"></div>
+        </div>
       </div>
 
-      <div></div>
+      <div className="flex flex-row gap-5 content-center items-center">
+        <FontAwesomeIcon
+          icon={faCirclePlay}
+          className="text-[#7d7d7d] cursor-pointer hover:text-white"
+        />
+        <FontAwesomeIcon
+          icon={faMicrophone}
+          className="text-[#7d7d7d] cursor-pointer  hover:text-white"
+        />
+        <FontAwesomeIcon
+          icon={faList}
+          className="text-[#7d7d7d] cursor-pointer  hover:text-white"
+        />
+        <FontAwesomeIcon
+          icon={faDesktop}
+          className="text-[#7d7d7d] cursor-pointer  hover:text-white"
+        />
+        <FontAwesomeIcon
+          icon={faVolumeHigh}
+          className="text-[#7d7d7d] cursor-pointer  hover:text-white"
+        />
+
+        <div className="w-[100px] h-[5px] bg-[#ccc]  rounded-full overflow-hidden">
+          <div className="w-[50%] h-full bg-white"></div>
+        </div>
+
+        <FontAwesomeIcon
+          icon={faUpRightAndDownLeftFromCenter}
+          className=" text-[#7d7d7d] cursor-pointer  hover:text-white"
+        />
+      </div>
     </div>
   );
 };
