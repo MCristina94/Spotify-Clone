6d93128603fe8c891cd5a4878d4854068f80226e
Maureen Garcia <cmaureen1908@gmail.com>
2025-03-27 16:30:04 -0500

    added center icons on footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,10 +2,17 @@ import React from "react";
 import imageSabrina from "../../public/images/sabrinacarpenter.jpeg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCirclePlay } from "@fortawesome/free-regular-svg-icons/faCirclePlay";
+import {
+  faShuffle,
+  faForward,
+  faBackward,
+  faRepeat,
+} from "@fortawesome/free-solid-svg-icons";
+import { faCirclePlay as playIcon } from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
   return (
-    <div className="h-[100px] p-5">
+    <div className="h-[100px] p-5 flex flex-row ">
       <div className="mx-5 flex flex-row">
         <img src={imageSabrina} className="w-[60px] h-[60px] rounded-md" />
         <div className="flex flex-col  mx-5 justify-center ">
@@ -18,6 +25,31 @@ const Footer = () => {
           </div>
         </div>
       </div>
+
+      <div className="flex flex-row gap-2 content-center items-center">
+        <FontAwesomeIcon
+          icon={faShuffle}
+          className=" text-[#2b2b35]  text-[20px] mx-3"
+        />
+        <FontAwesomeIcon
+          icon={faBackward}
+          className=" text-[#7d7d7d] text-[20px] mx-3"
+        />
+        <FontAwesomeIcon
+          icon={playIcon}
+          className=" text-white text-[30px] mx-3"
+        />
+        <FontAwesomeIcon
+          icon={faForward}
+          className=" text-[#7d7d7d] text-[20px] mx-3"
+        />
+        <FontAwesomeIcon
+          icon={faRepeat}
+          className=" text-[#2b2b35] text-[20px] mx-3"
+        />
+      </div>
+
+      <div></div>
     </div>
   );
 };
