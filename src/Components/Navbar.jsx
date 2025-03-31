b36d0ad632b05cdd868a370d393dd3e9af6ef372
Maureen Garcia <cmaureen1908@gmail.com>
2025-03-31 11:42:54 -0500

    add navbar responsiv

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,58 +7,84 @@ import {
   faCircleArrowDown,
 } from "@fortawesome/free-solid-svg-icons";
 import { faBell } from "@fortawesome/free-regular-svg-icons";
+import { useMediaQuery } from "react-responsive";
 
 const Navbar = () => {
+  const isMobile = useMediaQuery({ query: "(max-width: 600px)" });
   return (
-    <header className="flex flex-row justify-between  px-5 fixed bg-black w-[100%] z-50">
-      <div className="flex flex-row justify-between p-5">
-        <FontAwesomeIcon
-          icon={faSpotify}
-          style={{ color: "#ffffff", fontSize: "35px" }}
-        />
-      </div>
-      <div className="flex flex-row m-4 gap-2">
-        <div className="w-12 h-12 rounded-3xl content-center text-center bg-[#121212] hover:bg-[#2A2A2A] hover:scale-105 transition-transform cursor-pointer">
-          <FontAwesomeIcon
-            icon={faHouse}
-            className="text-[#7d7d7d] text-[20px] hover:text-white transition-colors duration-200 "
-          />
-        </div>
-        <div className="relative w-100">
-          <FontAwesomeIcon
-            icon={faMagnifyingGlass}
-            className="absolute left-3 top-1/2 transform -translate-y-1/2 text-[#7d7d7d] text-lg"
-          />
-          <input
-            type="text"
-            name="search"
-            id="search"
-            className="block w-full pl-10 pr-3 py-2.5 text-base placeholder:text-[#7d7d7d] focus:outline-none sm:text-sm rounded-3xl bg-[#121212] hover:bg-[#2A2A2A] border-2 border-transparent hover:border-white focus:border-white transition-all duration-200"
-            placeholder="¿Qué quieres reproducir?"
-          />
-        </div>
-      </div>
-      <div className="flex flex-row gap-5 items-center ">
-        <div className="flex flex-row gap-2 content-center hover:scale-105 cursor-pointer hover:text-white transition-colors ">
-          <FontAwesomeIcon
-            icon={faCircleArrowDown}
-            className="text-[#9a9a9a] text-[20px] hover:text-white transition-colors"
-          />
-          <h2 className="text-[#9a9a9a] text-[15px] hover:text-white transition-colors">
-            Instalar app
-          </h2>
-        </div>
-        <FontAwesomeIcon
-          icon={faBell}
-          className="text-[#9a9a9a] text-[20px] hover:text-white transition-colors mt-1 cursor-pointer"
-        />
-        <div className="w-12 h-12 flex items-center justify-center rounded-full bg-[#333030] text-black text-[15px] ">
-          <div className="w-8 h-8 flex items-center justify-center rounded-full bg-[#4d98f0] text-black text-[15px]">
-            MC
+    <>
+      {!isMobile ? (
+        <header className="flex flex-row justify-between  px-5 fixed bg-black w-[100%] z-50">
+          <div className="flex flex-row justify-between p-5">
+            <FontAwesomeIcon
+              icon={faSpotify}
+              style={{ color: "#ffffff", fontSize: "35px" }}
+            />
           </div>
-        </div>
-      </div>
-    </header>
+          <div className="flex flex-row m-4 gap-2">
+            <div className="w-12 h-12 rounded-3xl content-center text-center bg-[#121212] hover:bg-[#2A2A2A] hover:scale-105 transition-transform cursor-pointer">
+              <FontAwesomeIcon
+                icon={faHouse}
+                className="text-[#7d7d7d] text-[20px] hover:text-white transition-colors duration-200 "
+              />
+            </div>
+            <div className="relative w-100">
+              <FontAwesomeIcon
+                icon={faMagnifyingGlass}
+                className="absolute left-3 top-1/2 transform -translate-y-1/2 text-[#7d7d7d] text-lg"
+              />
+              <input
+                type="text"
+                name="search"
+                id="search"
+                className="block w-full pl-10 pr-3 py-2.5 text-base placeholder:text-[#7d7d7d] focus:outline-none sm:text-sm rounded-3xl bg-[#121212] hover:bg-[#2A2A2A] border-2 border-transparent hover:border-white focus:border-white transition-all duration-200"
+                placeholder="¿Qué quieres reproducir?"
+              />
+            </div>
+          </div>
+          <div className="flex flex-row gap-5 items-center ">
+            <div className="flex flex-row gap-2 content-center hover:scale-105 cursor-pointer hover:text-white transition-colors ">
+              <FontAwesomeIcon
+                icon={faCircleArrowDown}
+                className="text-[#9a9a9a] text-[20px] hover:text-white transition-colors"
+              />
+              <h2 className="text-[#9a9a9a] text-[15px] hover:text-white transition-colors">
+                Instalar app
+              </h2>
+            </div>
+            <FontAwesomeIcon
+              icon={faBell}
+              className="text-[#9a9a9a] text-[20px] hover:text-white transition-colors mt-1 cursor-pointer"
+            />
+            <div className="w-12 h-12 flex items-center justify-center rounded-full bg-[#333030] text-black text-[15px] ">
+              <div className="w-8 h-8 flex items-center justify-center rounded-full bg-[#4d98f0] text-black text-[15px]">
+                MC
+              </div>
+            </div>
+          </div>
+        </header>
+      ) : (
+        <header className="flex flex-row  py-5 px-5 fixed bg-black w-[100%] z-50 ">
+          {" "}
+          <div className="w-12 h-12 flex items-center justify-center rounded-full  text-black text-[15px] ">
+            <div className="w-8 h-8 flex items-center justify-center rounded-full bg-[#4d98f0] text-black text-[15px]">
+              MC
+            </div>
+          </div>
+          <div className="flex flex-row mb-3 mt-1 gap-2   ">
+            <div className=" bg-green-500 cursor-pointer p-2 px-3 rounded-3xl text-black">
+              <h3 className="text-[13px] font-light">Todo</h3>
+            </div>
+            <div className="  bg-[#282828] cursor-pointer p-2 px-3 rounded-3xl  ">
+              <h3 className="text-[13px] font-light">Música</h3>
+            </div>
+            <div className="  bg-[#282828] cursor-pointer p-2 px-3 rounded-3xl  ">
+              <h3 className="text-[13px] font-light">Pódcasts</h3>
+            </div>
+          </div>
+        </header>
+      )}
+    </>
   );
 };
 
